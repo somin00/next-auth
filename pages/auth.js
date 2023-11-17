de0a2a09cab1441c1d22d0e81ff9d2ce10fa83e1
somin <smhs12@naver.com>
2023-11-17 17:23:59 +0900

    feat: auth 페이지 리디렉션 기능 수정

--- pages/auth.js
+++ pages/auth.js
@@ -1,24 +1,27 @@
 import { getSession } from "next-auth/client";
 import AuthForm from "../components/auth/auth-form";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 function AuthPage() {
-  return <AuthForm />;
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>로딩중입니다.</p>;
+  }
+
+  return <>{!isLoading && <AuthForm />}</>;
 }
 
 export default AuthPage;
-
-export const getServerSideProps = async (context) => {
-  const session = getSession({ req: context.req });
-
-  if (session) {
-    return {
-      redirect: {
-        destination: "/profile",
-        permanent: false,
-      },
-    };
-  }
-  return {
-    props: { session },
-  };
-};
