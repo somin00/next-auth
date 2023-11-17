0f182369e855263afeef5e36675e0ad61461c7ff
somin <smhs12@naver.com>
2023-11-17 15:14:10 +0900

    feat: 인증페이지, 프로필 페이지 라우트 보호 기능 구현

--- pages/auth.js
+++ pages/auth.js
@@ -1,7 +1,24 @@
-import AuthForm from '../components/auth/auth-form';
+import { getSession } from "next-auth/client";
+import AuthForm from "../components/auth/auth-form";
 
 function AuthPage() {
   return <AuthForm />;
 }
 
 export default AuthPage;
+
+export const getServerSideProps = async (context) => {
+  const session = getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/profile",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+};
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,24 @@
-import UserProfile from '../components/profile/user-profile';
+import UserProfile from "../components/profile/user-profile";
+import { getSession } from "next-auth/client";
 
 function ProfilePage() {
   return <UserProfile />;
 }
 
 export default ProfilePage;
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false, //리디렉션 임시로 설정
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+};
